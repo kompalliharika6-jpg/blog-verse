5399391a793e6f632a006f765f176d954c9cb86e
kompalliharika6-jpg <kompalliharika6@gmail.com>
2025-09-06 12:38:37 +0530

    but we will continue attending the session without fail

--- practice.js
+++ practice.js
@@ -1,1 +1,40 @@
 console.log("hello world,harika")
+
+let name="harika";
+console.log(name)
+
+let num=18;
+console.log(num)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+
+let islogged=false
+console.log(islogged)
+
+console.log(typeof islogged)
+console.log(islogged)
+
+console.log(typeof islogged)
+console.log(typeof x)
+
+let arr=[1,2,4,5,6]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person={
+    name:"harika",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
+
+
+
